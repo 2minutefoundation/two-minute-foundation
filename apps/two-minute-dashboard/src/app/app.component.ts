9585c3fabd206472f86acb6f14a3b39b5a53ad03
Alan Beech <alan.beech-ext@aldautomotive.com>
2021-05-02 16:23:47 +0100

    initial commit

---
+++ apps/two-minute-dashboard/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'two-minute-foundation-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'two-minute-dashboard';
+}
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/two-minute-dashboard'],
+};
