3107d29eec5d1730ada93a925aabeb7d572cdab5
Alan Beech <appdev@2minute.com>
2021-05-08 16:46:52 +0100

    initial map setup

--- apps/two-minute-dashboard/src/app/app.module.ts
+++ apps/two-minute-dashboard/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { MarkerService } from './marker.service';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
-  providers: [],
+  imports: [BrowserModule, HttpClientModule],
+  providers: [MarkerService],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
